69d2237e92ed2d0dfe3c7889c82320a271f92853
Lancelot Edwards <lancelot.edwards@gmail.com>
2016-05-14 12:14:47 -0500

    react skeleton

---
+++ src/components/List.jsx
@@ -0,0 +1,20 @@
+var React = require('react');
+var ListItem = require('./ListItem.jsx');
+
+var ingredients = [
+  { id: 1, text: 'cheese' },
+  { id: 2, text: 'chicken breast' },
+  { id: 3, text: 'brown bread' },
+];
+
+var List = React.createClass({
+  render: function () {
+    var listItems = ingredients.map(function (item) {
+      return <ListItem key={item.id} ingredient={item.text} />;
+    });
+
+    return (<ul>{listItems}</ul>);
+  },
+});
+
+module.exports = List;
